aa029e98c340f72f631ae48fd890e682aadb8f9e
Jutta Hasselmann <59652188+JuttaHasselmann@users.noreply.github.com>
2020-02-03 18:03:17 +0100

    Added another Amen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Great is the Lord Jesus Christ! Amen Amen");
+	    System.out.println("Great is the Lord Jesus Christ! Amen Amen Amen");
     }
 }
